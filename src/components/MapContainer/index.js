970063babd5756b1fe99d9d46c8d78b1cab0b330
kimtimmy93 <kimtimmy93@gmail.com>
2019-12-08 23:24:40 -0800

    add enter location

--- src/components/MapContainer/index.js
+++ src/components/MapContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker } from "react-google-maps";
+import Autocomplete from 'react-google-autocomplete'
 import Geocode from "react-geocode";
 Geocode.setApiKey("AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0");
 Geocode.enableDebug();
@@ -164,6 +165,30 @@ constructor( props ){
      }
     );
    };
+   onPlaceSelected = ( place ) => {
+    const address = place.formatted_address,
+       addressArray =  place.address_components,
+       city = this.getCity( addressArray ),
+       area = this.getArea( addressArray ),
+       state = this.getState( addressArray ),
+       latValue = place.geometry.location.lat(),
+       lngValue = place.geometry.location.lng();
+    // Set these values in the state.
+      this.setState({
+       address: ( address ) ? address : '',
+       area: ( area ) ? area : '',
+       city: ( city ) ? city : '',
+       state: ( state ) ? state : '',
+       markerPosition: {
+        lat: latValue,
+        lng: lngValue
+       },
+       mapPosition: {
+        lat: latValue,
+        lng: lngValue
+       },
+      })
+    };
 render(){
 const AsyncMap = withScriptjs(
    withGoogleMap(
@@ -172,6 +197,17 @@ const AsyncMap = withScriptjs(
       defaultZoom={this.props.zoom}
       defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
      >
+          <Autocomplete
+       style={{
+        width: '100%',
+        height: '40px',
+        paddingLeft: '16px',
+        marginTop: '2px',
+        marginBottom: '100px'
+       }}
+       onPlaceSelected={ this.onPlaceSelected }
+       types={['(regions)']}
+      />
          <InfoWindow
        onClose={this.onInfoWindowClose}
        position={{ lat: ( this.state.markerPosition.lat + 0.0018 ), lng: this.state.markerPosition.lng }}
--- src/components/Marker/index.js
+++
@@ -1,9 +0,0 @@
-import { Marker } from "react-google-maps";
-
-    //   <Marker google={this.props.google}
-    //    name={'Dolores park'}
-    //       draggable={true}
-    //       onDragEnd={ this.onMarkerDragEnd }
-    //          position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
-    //   />
-    //   <Marker />
