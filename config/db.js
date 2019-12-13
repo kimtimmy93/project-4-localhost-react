0a13840569b16fc727e18e0bb666d3d0efa5c8bb
kimtimmy93 <kimtimmy93@gmail.com>
2019-12-13 13:26:38 -0800

    mongodburi

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost/localhost';
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, { useNewUrlParser: true,
                                      useUnifiedTopology: true,
